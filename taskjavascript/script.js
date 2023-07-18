e13453c6ea634c23cbd6d3348fb8007c85156ec9
hamza016 <syedhamza016@gmail.com>
2023-07-18 20:40:17 +0500

    Module 1 without create functionality

--- taskjavascript/script.js
+++ taskjavascript/script.js
@@ -1,4 +1,3 @@
-
 // var array = fetch('./data.json')
 //     .then((response) => response.json())
 //     .then((json) => console.log(json));
@@ -8,12 +7,12 @@ const array = [
     {
         "UserId" : 1,
         "UserName": "John",
-        "DOB" : "23031995",
+        "DOB" : "23/03/1995",
         "Location" :"Lahore",
         "MobileNumber" : "03331549410",
         "Gender" : "Male",
         "Height" : "5'7",
-        "Weight" : "70kg"
+        "Weight" : 70
     },
     {
         "UserId" : 2,
@@ -23,7 +22,7 @@ const array = [
         "MobileNumber" : "0334349410",
         "Gender" : "Male",
         "Height" : "5'1",
-        "Weight" : "51kg"
+        "Weight" : 51
     },{
         "UserId" : 3,
         "UserName": "Waqar",
@@ -32,7 +31,7 @@ const array = [
         "MobileNumber" : "03317673347",
         "Gender" : "Male",
         "Height" : "5'8",
-        "Weight" : "83kg"
+        "Weight" : 83
     },{
         "UserId" : 4,
         "UserName": "Junaid",
@@ -41,7 +40,7 @@ const array = [
         "MobileNumber" : "03336617181",
         "Gender" : "Male",
         "Height" : "6'2",
-        "Weight" : "91kg"
+        "Weight" : 91
     },{
         "UserId" : 5,
         "UserName": "Tahir",
@@ -50,7 +49,7 @@ const array = [
         "MobileNumber" : "03214891234",
         "Gender" : "Male",
         "Height" : "5'2",
-        "Weight" : "56kg"
+        "Weight" : 56
     },{
         "UserId" : 6,
         "UserName": "Talha",
@@ -59,7 +58,7 @@ const array = [
         "MobileNumber" : "03156320620",
         "Gender" : "Male",
         "Height" : "5'6",
-        "Weight" : "71kg"
+        "Weight" : 71
     },{
         "UserId" : 7,
         "UserName": "Saad",
@@ -68,7 +67,7 @@ const array = [
         "MobileNumber" : "03326437330",
         "Gender" : "Male",
         "Height" : "5'10",
-        "Weight" : "84kg"
+        "Weight" : 84
     },{
         "UserId" : 8,
         "UserName": "Zohair",
@@ -77,7 +76,7 @@ const array = [
         "MobileNumber" : "03003478142",
         "Gender" : "Male",
         "Height" : "5'4",
-        "Weight" : "66kg"
+        "Weight" : 66
     },{
         "UserId" : 9,
         "UserName": "Carlos",
@@ -86,7 +85,7 @@ const array = [
         "MobileNumber" : "03096754348",
         "Gender" : "Male",
         "Height" : "5'2",
-        "Weight" : "70kg"
+        "Weight" : 70
     },{
         "UserId" : 10,
         "UserName": "Yousaf",
@@ -95,7 +94,7 @@ const array = [
         "MobileNumber" : "03435692340",
         "Gender" : "Male",
         "Height" : "5'9",
-        "Weight" : "95kg"
+        "Weight" : 95
     },{
         "UserId" : 11,
         "UserName": "Abdullah",
@@ -104,15 +103,107 @@ const array = [
         "MobileNumber" : "03355237723",
         "Gender" : "Male",
         "Height" : "5'4",
-        "Weight" : "59kg"
+        "Weight" : 59
     }
-]
+];
+
+//<<<<<<<<<<<<<<----------Create user----------->>>>>>>>>>>
+// function addUser()
+// {
+//     array.push
+
+// }
+
+//<<<<<<<<<<<<<<----------Update user----------->>>>>>>>>>>
+
+
+function updateUserByUsername(usernameToUpdate, newValues) {
+    const lowercaseUsername = usernameToUpdate.toLowerCase();
+    const updatedArray = array.map((user) => {
+        if (user.UserName.toLowerCase() === lowercaseUsername) {
+            return { ...user, ...newValues }; // Merge the existing user object with the new values
+        }
+        return user;
+    });
+    return updatedArray;
+}
 
 
 
+// // Example usage:
+const usernameToUpdate = 'John';
+const newValues = { Location: 'New York', Weight: 75 };
+const newArrayWithUpdatedUser = updateUserByUsername(usernameToUpdate, newValues);
+
+console.log('Updated Array:', newArrayWithUpdatedUser);
+
+
+//<<<<<<<<<<<<<-----------Delete user----------->>>>>>>>>>>
+
+function deleteUserByUsername(usernameToDelete) {
+        const lowercaseUsername = usernameToDelete.toLowerCase();
+        const filteredArray = array.filter((user) => {
+            return user.UserName.toLowerCase() !== lowercaseUsername;
+        });
+    
+        return filteredArray;
+    }
+    
+    // Example usage:
+    const usernameToDelete = 'John';
+    const newArrayWithoutUser = deleteUserByUsername(usernameToDelete);
+    
+//<<<<<<<<<<<<<-----------Get user age---------->>>>>>>>>>>
+
+function parseDate(dateStr) {
+    // Custom function to parse different date formats
+    const formatsToTry = ["DD-MM-YYYY", "DD/MM/YYYY"];
+    for (const format of formatsToTry) {
+        const parts = dateStr.split(/[-/]/);
+        if (parts.length === 3 && parts[0].length === 2 && parts[1].length === 2 && parts[2].length === 4) {
+            return new Date(parts[2], parts[1] - 1, parts[0]);
+        }
+    }
+    return null; // Invalid date format
+}
+
+function age(username) {
+    const user = array.find((user) => user.UserName === username);
+    if (!user) {
+        console.log('User not found.');
+        return;
+    }
+
+    const dob = parseDate(user.DOB);
+    if (!dob) {
+        console.log('Invalid date format for Date of Birth.');
+        return;
+    }
+
+    const currentDate = new Date();
+    const ageInMilliseconds = currentDate - dob;
+    const ageDate = new Date(ageInMilliseconds); // Convert the difference to a date object
+    const ageInYears = ageDate.getUTCFullYear() - 1970;
+    const ageInMonths = ageDate.getUTCMonth();
+
+    console.log('Your Age is:');
+    console.log(
+        ageInYears + ' Years ' +
+        ageInMonths + ' Months ' +
+        ageDate.getUTCDate() + ' Days'
+    );
+}
+
+// Example usage:
+age('John');
+
+
+//<<<<<<<<<<<<-----------Get all users--------->>>>>>>>>>>>
+//<<<<<<<<<<<--------Ascending/Descending------>>>>>>>>>>>>
+//<<<<<<<<<<<---Search on basis of properties-->>>>>>>>>>>>
 // //Comparer Function    
-function GetSortOrder(prop) {    
-    return function(a, b) {    
+function GetSortOrderAsc(prop) {    
+    return function(a, b) { 
         if (a[prop] > b[prop]) {    
             return 1;    
         } else if (a[prop] < b[prop]) {    
@@ -121,9 +212,34 @@ function GetSortOrder(prop) {
         return 0;    
     }    
 }    
+function GetSortOrderDesc(prop) {    
+    return function(a, b) { 
+        if (a[prop] < b[prop]) {    
+            return 1;    
+        } else if (a[prop] > b[prop]) {    
+            return -1;    
+        }    
+        return 0;    
+    }    
+}    
 
-function GetAllUser()
-{
-    return JSON.parse.array;
+
+// Find any user by username
+function findUserByUsername(usernameToFind) {
+    const lowercaseUsername = usernameToFind.toLowerCase();
+    const user = array.find((user) => {
+        return user.UserName.toLowerCase() === lowercaseUsername;
+    });
+
+    return user || null;
 }
-console.log(array.sort(GetSortOrder("Experience"))); 
+
+// Example usage:
+const usernameToSearch = 'tahir';
+const foundUser = findUserByUsername(usernameToSearch);
+
+if (foundUser) {
+    console.log('User found:', findUserByUsername(foundUser.UserName));
+} else {
+    console.log('User not found.');
+}
